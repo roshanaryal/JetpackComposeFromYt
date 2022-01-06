8a656a32b0f06af061b6d91e7d9dbc1f0f281c5c
Roshan Aryal <roshanar.dev@gmail.com>
2022-01-06 17:43:37 +0545

    Added ImageBox

--- app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
+++ app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
@@ -3,17 +3,26 @@ package com.aarappstudios.jetpackcomposefromyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aarappstudios.jetpackcomposefromyt.ui.theme.JetpackComposeFromYtTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,22 +36,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainDemo() {
-    ModiFierDemo()
+//    ModiFierDemo()
+    val painter= painterResource(id = R.drawable.demo)
+    val modifier=Modifier.offset(10.dp,10.dp)
+
+    Row() {
+        Box(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(10.dp))
+        {
+            ImageCard(painter = painter,"Demo Text","Demo image",modifier)
+
+        }
+
+        Box(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(10.dp))
+        {
+            ImageCard(painter = painter,"Demo Text","Demo image",modifier)
+
+        }
+    }
+
 }
+
 
 @Composable
 fun ModiFierDemo() {
 
-    Column(modifier = Modifier
-        .background(Color.Green)
-        .fillMaxWidth()
-        .fillMaxHeight(0.5f)
-        .border(1.dp,Color.Magenta)
-        .padding(50.dp)
-        .border(1.dp,Color.Magenta)
+    Column(
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxWidth()
+            .fillMaxHeight(0.5f)
+            .border(1.dp, Color.Magenta)
+            .padding(50.dp)
+            .border(1.dp, Color.Magenta)
 //        .requiredWidth()
     ) {
-        Text(text = "Hello",modifier = Modifier
+        Text(
+            text = "Hello", modifier = Modifier
 //            .offset(40.dp,30.dp)
 
         )
@@ -53,28 +86,50 @@ fun ModiFierDemo() {
 }
 
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(), shape = RoundedCornerShape(15.dp), elevation = 5.dp
+
+    ) {
+//        modifier.border(1.dp, Color.Red)
+        Box(modifier = Modifier.height(200.dp)) {
+
+            Image(painter = painter, contentDescription =contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxHeight()
+                )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.Black), 300f
+                    )
+                )
+            ) {
 
 
+            }
+            
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+            ,contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title
+                ,style = TextStyle(color = Color.White,fontSize = 16.sp)
+                )
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 @Composable
