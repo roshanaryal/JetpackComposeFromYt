82613ce49d9e18c2539d321982a23398620a7681
Roshan Aryal <roshanar.dev@gmail.com>
2022-01-06 16:48:30 +0545

    modifier

--- app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
+++ app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.aarappstudios.jetpackcomposefromyt
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
@@ -20,26 +20,81 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier.background(color = Color.LightGray)
-                    .fillMaxWidth()
-                    .height(100.dp)
-                    .padding(8.dp)
-                    .clickable {
-                               Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
-                    }
-
-                ,
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Greeting(name = "Hello")
-                Greeting(name = "World")
-                Greeting(name = "Nepal")
-
-            }
-
+            MainDemo()
         }
+    }
+}
+
+@Composable
+fun MainDemo() {
+    ModiFierDemo()
+}
+
+@Composable
+fun ModiFierDemo() {
+
+    Column(modifier = Modifier
+        .background(Color.Green)
+        .fillMaxWidth()
+        .fillMaxHeight(0.5f)
+        .border(1.dp,Color.Magenta)
+        .padding(50.dp)
+        .border(1.dp,Color.Magenta)
+//        .requiredWidth()
+    ) {
+        Text(text = "Hello",modifier = Modifier
+//            .offset(40.dp,30.dp)
+
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(text = "World")
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Composable
+fun RowDemo() {
+    Row(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(8.dp)
+            .clickable {
+//                Toast.makeText(MainActivity@,"Clicked",Toast.LENGTH_SHORT).show()
+            },
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Greeting(name = "Hello")
+        Greeting(name = "World")
+        Greeting(name = "Nepal")
+
     }
 }
 
@@ -52,6 +107,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeFromYtTheme {
-        Greeting("Android")
+        MainDemo()
     }
 }
