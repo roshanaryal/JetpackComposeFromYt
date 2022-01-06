daab15fb698fbf68775f7ce17e411a6f76451bf6
Roshan Aryal <roshanar.dev@gmail.com>
2022-01-06 16:13:43 +0545

    finished row and column

--- app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
+++ app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
@@ -1,30 +1,37 @@
 package com.aarappstudios.jetpackcomposefromyt
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aarappstudios.jetpackcomposefromyt.ui.theme.JetpackComposeFromYtTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            Row(
                 modifier = Modifier.background(color = Color.LightGray)
-                    .fillMaxSize()
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(8.dp)
+                    .clickable {
+                               Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+                    }
+
                 ,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Greeting(name = "Hello")
                 Greeting(name = "World")
