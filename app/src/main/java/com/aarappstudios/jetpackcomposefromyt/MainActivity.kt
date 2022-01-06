f05da4c8d836d66e5caeb7bf3d0887fb0862d815
Roshan Aryal <roshanar.dev@gmail.com>
2022-01-06 14:18:38 +0545

    comumn finished

--- app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
+++ app/src/main/java/com/aarappstudios/jetpackcomposefromyt/MainActivity.kt
@@ -3,10 +3,15 @@ package com.aarappstudios.jetpackcomposefromyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.aarappstudios.jetpackcomposefromyt.ui.theme.JetpackComposeFromYtTheme
 
@@ -14,7 +19,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Greeting(name="Hello")
+            Column(
+                modifier = Modifier.background(color = Color.LightGray)
+                    .fillMaxSize()
+                ,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Greeting(name = "Hello")
+                Greeting(name = "World")
+                Greeting(name = "Nepal")
+
+            }
+
         }
     }
 }
